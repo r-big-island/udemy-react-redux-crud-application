abbb48de68b0f8e32458fb228aec1ca897d509f9
r-big-island <lalaknockrara@gmail.com>
2021-08-04 17:51:02 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React, {  } from 'react';
 
 function App() {
   return (
-    <div><h1>Hello, world!</h1></div>
-  );
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onClick={() => console.log("I am clicked")}></input>
+    </React.Fragment>
+  )
+  
 }
 
 export default App;
