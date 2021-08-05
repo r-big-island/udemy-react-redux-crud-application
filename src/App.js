70186aca6e1c91ab04d803c444218a02907e561c
r-big-island <lalaknockrara@gmail.com>
2021-08-05 13:50:13 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 3 }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
+class Counter extends Component {
+  // 初期化処理時に実行されるメソッド
+  // ⇨Counterクラス呼び出し時に必ず最初に実行される
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    // 状態の初期定義はConstructor内で行う
+    this.state = { count: 0 }
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  handlePlusButton = () => {
+    // const count = this.state.count
+    // メソッド内で一度しか使わない値は変数定義しない（リファクタリングの考え方）
+    // stateの変更はsetState（Reactの標準関数）を使う
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+
+  // Component拡張クラスの時はrender関数を使う
+  render() {
+    console.log("render")
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
